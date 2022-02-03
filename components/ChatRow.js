4656e933d0f69dcca2a46e8965148072fa055691
Vaibhav Singh Bhadouria <86617405+Vs153698@users.noreply.github.com>
2022-02-03 13:30:20 +0530

    tinder clone completed

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import useAuth from './../hooks/useAuth'
 import tw from 'tailwind-react-native-classnames';
 import getMatchedUserInfo from '../lib/getMatchedUserInfo';
+import { onSnapshot, orderBy, query,collection } from 'firebase/firestore';
+import { db } from '../firebase';
 const ChatRow = ({ item, navigation }) => {
   const [matchedUserInfo, setMatchedUserInfo] = useState(null);
   const [lastMessage, setLastMessage] = useState(null);
@@ -10,6 +12,10 @@ const ChatRow = ({ item, navigation }) => {
   useEffect(() => {
     setMatchedUserInfo(getMatchedUserInfo(item.users, Currentuser.uid))
   }, [item, Currentuser]);
+  useEffect(() => onSnapshot(query(collection(db, 'matches',item.id, 'messages')),orderBy('timestamp', 'desc'),snapshot => setLastMessage(snapshot.docs[0]?.data()?.message))
+    
+  , [item,db]);
+  
   return (
     <TouchableOpacity onPress={()=>navigation.navigate('messagescreen',{item})} activeOpacity={0.6} style={[tw`flex-row items py-3 px-5 bg-white mx-3 my-1 rounded-lg`,styles.cardShadow]}  >
       <Image style={tw`rounded-full h-16 w-16 mr-4 `} source={{ uri: matchedUserInfo?.photoURL }} />
